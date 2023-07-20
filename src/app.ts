0cff48a7d1ee18a5c1c14e9620a868e905e29764
BekLg <bereketlegesse22@gmail.com>
2023-07-20 19:34:10 +0300

    created a=basic routes

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-console.log("test")
+
+import express, { Request, Response } from 'express';
+const app= express();
+const bcrypt= require("bcrypt")
+
+app.use(express.json());
+
+
+app.post("/register", (req : Request,res : Response)=>{
+    res.json("register");
+});
+
+app.post('/login', (req: Request, res: Response) => {
+    res.json('login');
+  });
+
+app.post("/profile", (req : Request,res : Response)=>{
+    res.json("profile");
+});
+
+
+app.listen(3000,()=>{
+    console.log("app is running on port 3000");
+    
+})
