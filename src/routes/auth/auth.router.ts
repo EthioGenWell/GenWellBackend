58217a1f685d17e711ae82ce2366a997adbee70f
BekLg <bereketlegesse22@gmail.com>
2023-07-21 13:22:18 +0300

    imported and connected the function with /login route

--- src/routes/auth/auth.router.ts
+++ src/routes/auth/auth.router.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { htmlRegisterUser } from './auth.controller';
+import { htmlLoginUser } from './auth.controller';
 
 const authRouter = express.Router();
 
 authRouter.post('/signup', htmlRegisterUser);
+authRouter.post('/login', htmlLoginUser);
 
 export default authRouter;
