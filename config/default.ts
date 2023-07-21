ebfb70fde1da6adb5a2b621463391a3fde6032af
Yagth <yabuman46@gmail.com>
2023-07-21 09:50:45 +0300

    Removed Unused variables

--- config/default.ts
+++ config/default.ts
@@ -6,10 +6,6 @@ const dev = {
   app: {
     host: 'localhost',
     port: process.env.SERVER_PORT || 8001,
-    REDIS_URL:
-      process.env.REDIS_URL ||
-      `${process.env.REDIS_HOST}://${process.env.REDIS_USER}:${process.env.REDIS_PORT}`,
-    NODE_ENV: process.env.NODE_ENV,
   },
   db: {
     username: process.env.MONGO_USERNAME,
