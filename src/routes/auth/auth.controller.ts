6e5746b61dc3f7f618890fb29b517d35809f38ba
Yagth <yabuman46@gmail.com>
2023-07-21 10:42:13 +0300

    Fixing errors

--- src/routes/auth/auth.controller.ts
+++ src/routes/auth/auth.controller.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from 'express';
 import { userDoc } from '../../modules/user';
-import bcrypt from 'bcrypt';
 import User from '../../modules/user';
+import bcrypt from 'bcrypt';
 
 export async function htmlRegisterUser(req: Request, res: Response) {
   const { email, password } = req.body;
 
   User.find({ email: email })
-    .then(async (foundUser: userDoc | null) => {
+    .then(async (foundUser) => {
       if (Array.isArray(foundUser) && foundUser.length === 0) {
         const hashedPassword = bcrypt.hashSync(password, 10);
         const user = new User({
